d13a8380a8b8d6dc010fe9a76cc32ce9ad3fd940
Federico Gonzalez <federico@innku.com>
2012-09-09 19:09:10 +1200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiCongreso::Application.config.secret_token = 'add97653f6f056ee7cc2d9cd2ad529a79bfeff3493bfbd9cbe148be40332558c378e2eff536153efd4d337d9aceb40ceefbd614809823e08af199ce66f728a48'
