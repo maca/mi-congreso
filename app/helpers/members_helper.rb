7b777dafd068a761b45f6890b4c05b27ca462e32
Federico Gonzalez <federico@innku.com>
2012-10-13 00:20:08 +1300

    Display initiative information

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -3,4 +3,8 @@ module MembersHelper
   def link_to_twitter_username(username)
     link_to "@#{username}", "http://www.twitter.com/#{username}", target: "blank"
   end
+
+  def party_abbr(member)
+    member.party.try(:short_name).to_s.downcase
+  end
 end
