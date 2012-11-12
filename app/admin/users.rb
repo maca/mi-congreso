03be6a2eb7d8b0068ed53401f4b232afa7fe32e5
Federico Gonzalez <federico@innku.com>
2012-11-12 22:10:18 +1300

    Add user to admin dashboard

---
+++ app/admin/users.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register User do
+
+  index do
+    column :id
+    column :name
+    column :email
+    default_actions
+  end
+
+  filter :name
+  filter :email
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs "Usuario" do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+end
