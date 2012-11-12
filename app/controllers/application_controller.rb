39fa8cefccecfa5ba66ef2a2a684b2f3b5746aec
Federico Gonzalez <federico@innku.com>
2012-11-12 22:44:30 +1300

    Remove pjax completely

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  layout :set_layout
-
-  private
-  def set_layout
-    if request.headers['X-PJAX']
-      false
-    else
-      "application"
-    end
-  end
 end
